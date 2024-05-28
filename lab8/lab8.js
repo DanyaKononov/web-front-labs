561cb3f3ddea889a92aac64692d5cc3e815e5fb7
unknown <danyakononov2004@gmail.com>
2024-05-29 05:30:54 +0700

    вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -12,4 +12,17 @@ function showDate() {
   outS.innerHTML =  "Дата и время для испанской локали: "+today.toLocaleString("es-ES");
   outF.innerHTML =  "Дата и время для французской локали: "+today.toLocaleString("fr-FR");
   outI.innerHTML =  "Дата и время для индонезийской локали: "+today.toLocaleString("id-ID");
+
+	let year = document.getElementById('outputYear');
+  let month = document.getElementById('outputMonth');
+  let day = document.getElementById('outputDay');
+  let weak = document.getElementById('outputWeak');
+  dt = new Date()
+  const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+  const weakkk = ['Воскресенье', "Понедельник", "Вторник","Среда","Четверг","Пятница","Суббота"]
+  year.innerHTML = "Текущий год: " + dt.getFullYear()
+  month.innerHTML = "Текущий месяц: " + months[dt.getMonth()];
+  day.innerHTML = "Текущий день: " + dt.getDate();
+  weak.innerHTML = "Текущий день недели: " + weakkk[dt.getDay()];
 }
+
---
+++ lab8/nowDate.js
@@ -0,0 +1,6 @@
+function showDate() {
+	const weekDays = ['Воскресенье', "Понедельник", "Вторник","Среда","Четверг","Пятница","Суббота"]
+	let dayNumber = dt.getDay()
+	
+
+}
