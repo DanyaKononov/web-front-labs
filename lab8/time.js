2969beea5e45cb7b9e73b3ac07d8d92f3e526749
Данил Кононов <danyakononov2004@gmail.com>
2024-05-29 18:45:15 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -4,6 +4,8 @@ function showTime() {
 	document.getElementById('time').innerHTML = currentTime
 	let currentTime2 = today.getSeconds()
 	document.getElementById('seconds').innerHTML = currentTime2
+	let time = document.getElementById('timeArrow')
+	time.style.transform = `rotate(${new Date().getSeconds() *  6}deg) translate(0, -50px)`;
 	
 }
 setInterval(showTime, 1000);
